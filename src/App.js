101a023c5ecc8eb7ec61e02ea06d4dbc8c791af7
Vibhu0555 <vaibhav.singh@pravisht.com>
2022-12-22 19:31:10 +0530

    this is first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import Card from './Components/Card';
-
+import {robots} from './Assets/robots';
+import SearchBox from './Components/SearchBox';
+import Cardlist from './Components/Cardlist';
+import { useState } from 'react';
 function App() {
+  const [searchRobo,setsearchRobo] = useState("");
+  const [stateRobos,setStateRobos] = useState(robots);
+
+  const onSearchChange=(event )=>{
+    setsearchRobo(event.target.value);
+    console.log(searchRobo);
+  }
+  const filteredrobot = stateRobos.filter(stateRobos=>{
+    return stateRobos.name.toLowerCase().includes(searchRobo.toLowerCase())
+  })
   return (
-        <>
-        <Card/>
-        </>
+    <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+        <Cardlist robots={filteredrobot} />
+    </div>   
   );
 }
 
